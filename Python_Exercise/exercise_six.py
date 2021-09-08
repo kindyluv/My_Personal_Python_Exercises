b082a34b7225c04dcb0ccc8d3fe41cb0e75e564b
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-08 13:20:08 +0100

    Add files via upload

---
+++ Python_Exercise/exercise_six.py
@@ -0,0 +1,11 @@
+is_pass = False
+while True:
+
+    if is_pass:
+        print('passed the exercise')
+        break
+    elif not is_pass:
+        print("Failed all exercise")
+        break
+    else:
+        print("you're a dumb_ass")
