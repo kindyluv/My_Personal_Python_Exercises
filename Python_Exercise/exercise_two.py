8d2fb40b5623d145d81d6a2ce461e4646eb1a951
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-08 13:21:52 +0100

    Add files via upload

---
+++ Python_Exercise/exercise_two.py
@@ -0,0 +1,7 @@
+first_name = 'olatoye'
+second_name = 'david'
+third_name = 'daramola'
+message = f'{first_name} {second_name} is a [{third_name}]'
+print(message.upper())
+print(len(first_name + second_name + third_name))
+
