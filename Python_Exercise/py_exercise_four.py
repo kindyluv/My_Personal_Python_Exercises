3208c691222562bfb6a496a13668f3cd800b3071
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-08 13:13:48 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_four.py
@@ -0,0 +1,8 @@
+name = input("Enter lenght of name:  ")
+
+if len(name) < 3:
+    print("name must be at least 3 characters")
+elif len(name) > 50:
+    print("name can be a maximum of 50 characters")
+else:
+    print("name looks good!")
