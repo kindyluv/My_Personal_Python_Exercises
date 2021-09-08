74ee76d7c249501ea1c861929209062bd44785a7
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-08 13:14:15 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_three.py
@@ -0,0 +1,7 @@
+temperature =int(input("Enter Temperature:  "))
+if temperature > 30:
+    print("It's a hot day")
+elif temperature < 10:
+    print("It's a cold day")
+else:
+    print("it's neither hot nor cold")
