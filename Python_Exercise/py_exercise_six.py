729951239859dafc65576c9a5fdf4750c57fbd92
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-09 10:51:52 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_six.py
@@ -0,0 +1,5 @@
+print("Hello " + input("whats is your name?:  "))
+
+first_number = input("enter two digit number for number one : ")
+second_number = input("enter two digit number for number two : ")
+print(first_number[0], second_number[1])
