d67447b7ad825b11eab63b6bf559b5e50add5843
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-09 17:08:23 +0100

    Add files via upload

--- Python_Exercise/py_exercise_eight.py
+++ Python_Exercise/py_exercise_eight.py
@@ -1,12 +1,25 @@
 import operator
 
-sign_operator = {
-    "*": operator.mul
-}
-current_age = int(input("Enter your current: "))
-years = 90 - current_age
-days = 356 * years
-week = 52 * years
-month = 12 * years
-result = f"You have {days} days, {week} weeks, {month} months, and {years} years left"
-print(result)
+
+def age():
+    global years, days, week, month
+    sign_operator = {
+        "*": operator.mul,
+    }
+    signs_operator = {
+        "-": operator.sub,
+    }
+    for sign in sign_operator:
+        future_age = int(input("Enter your future age: "))
+        current_age = int(input("Enter your current age: "))
+        years = sign_operator[sign](future_age, current_age)
+    for signs in signs_operator:
+        days = signs_operator[signs](365, years)
+        week = signs_operator[signs](52, years)
+        month = signs_operator[signs](12, years)
+    result = f"You have {days} days, {week} weeks, {month} months, and {years} years left"
+    print(result)
+
+
+age()
+
