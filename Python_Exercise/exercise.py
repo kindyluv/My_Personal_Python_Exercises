8f69d8eb771001ccf2302e8e6f419d818236b81c
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-10 17:05:01 +0100

    Add files via upload

--- Python_Exercise/exercise.py
+++ Python_Exercise/exercise.py
@@ -2,15 +2,8 @@ import math
 
 
 # number1
-def maximum_of_three_numbers():
-    number_0ne = int(input("Enter first number:  "))
-    number_two = int(input("Enter second number:  "))
-    number_three = int(input("Enter third number: "))
-
-    get_maximum_number = max(number_0ne, number_two, number_three)
-    get_minimum_number = min(number_0ne, number_two, number_three)
-    print("The Maximum number is ", get_maximum_number)
-    print("The minimum number is ", get_minimum_number)
+def maximum_of_three_numbers(first, second, third):
+    print(max(first, second, third))
 
 
 # number2
