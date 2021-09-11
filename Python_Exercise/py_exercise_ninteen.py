e1b9a6705250ed1e44c3ac5bcde941612358f1e5
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 19:06:24 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_ninteen.py
@@ -0,0 +1,10 @@
+def leap_year():
+    year = int(input("Enter year:  "))
+
+#     if year % 4 == 0:
+#         print(f"{year} a leap year")
+#     else:
+#         print(f"{year} is not a leap year")
+#
+#
+# leap_year()
