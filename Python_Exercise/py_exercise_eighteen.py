aaa18b77d3aec2016ea983bdfe850cede1845ee0
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 19:03:02 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_eighteen.py
@@ -0,0 +1,18 @@
+def user_bmi():
+    weight = float(input("Enter weight in m:  "))
+    height = float(input("Enter height in kg:  "))
+
+#     bmi = weight/height**2
+#     if bmi < 18.5:
+#         print(f"your bmi is {bmi} so you are underweight.")
+#     elif 18.5 < bmi < 25:
+#         print(f"your bmi is {bmi} so you are  normal weight.")
+#     elif 25 < bmi < 30:
+#         print(f"your bmi is {bmi} so you are  overWeight.")
+#     elif 30 < bmi < 35:
+#         print(f"your bmi is {bmi} so you are  obese.")
+#     else:
+#         print(f"your bmi is {bmi} so you are  clinically obese.")
+#
+#
+# user_bmi()
