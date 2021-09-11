09eff2ca6f5786835d14dc97ea292751c97ba0f9
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 18:52:20 +0100

    Add files via upload

--- Python_Exercise/py_exercise_sixteen.py
+++ Python_Exercise/py_exercise_sixteen.py
@@ -1,3 +1,7 @@
 def rollercoaster_two():
     height = int(input("Enter height:  "))
 
+    if height > 120:
+        print("welcome to semicolon roller coaster")
+        age = int(input("kindly enter your age:  "))
+      
