4b9a87b8b65ddf9c3c4ae06317a885e5809871b3
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 20:28:59 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_twenty_one.py
@@ -0,0 +1,26 @@
+def rollercoaster_four():
+    height = int(input("Enter height:  "))
+    cost = 0
+#     if height > 120:
+#         print("welcome to semicolon roller coaster")
+#         age = int(input("kindly enter your age:  "))
+#         if age > 18:
+#             cost = 12
+#             print(f"adult ride would cost you ${cost}")
+#         elif 18 >= age >= 12:
+#             cost = 7
+#             print(f"teenage ride would cost you ${cost}")
+#         else:
+#             cost = 5
+#             print(f"child ride would cost you $[{cost}")
+#         want_photos = input("Enter Yes or No:  ")
+#         if want_photos == "yes":
+#             cost += 3
+#             print(f"your bill is additional ${cost}")
+#         else:
+#             print(f"your bill is {cost}")
+#     else:
+#         print("sorry you are not qualified to ride the rollercoaster")
+#
+#
+# rollercoaster_four()
