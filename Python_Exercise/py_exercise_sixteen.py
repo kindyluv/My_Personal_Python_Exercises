f38ac946b2f4e749e55cf2b97ca9d102b14cedfd
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 18:56:03 +0100

    Add files via upload

--- Python_Exercise/py_exercise_sixteen.py
+++ Python_Exercise/py_exercise_sixteen.py
@@ -4,8 +4,9 @@ def rollercoaster_two():
     if height > 120:
         print("welcome to semicolon roller coaster")
         age = int(input("kindly enter your age:  "))
-    if age > 18:
+    if age >= 18:
         print("your ride would cost you $12")
-    else:
+    elif age < 18:
         print("your ride would cost you $7")
-
+    else:
+        print("sorry you are not qualified to ride the rollercoaster")
