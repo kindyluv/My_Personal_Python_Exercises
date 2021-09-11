f68ec41c4793e045714fad0a810ee35a1942af36
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 20:35:26 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_twenty_two.py
@@ -0,0 +1,43 @@
+def pizza_deliveries():
+    print("Welcome to precious pizza deliveries!")
+    pizza_size = input("What size of pizza do want? small, medium and large:  ")
+
+    small_size = 15
+    medium_size = 20
+    large_size = 25
+#
+#     if pizza_size == "small":
+#         print(f"your pizza bill is {small_size}")
+#     elif pizza_size == "medium":
+#         print(f"your pizza bill is {medium_size}")
+#     elif pizza_size == "large":
+#         print(f"your pizza bill is {large_size}")
+#     else:
+#         print("no under wetin you want oh")
+#     add_pepperoni = input("Do you want pepperoni? yes or no  ")
+#     if add_pepperoni == "yes":
+#         small_size += 2
+#         print(f"your final bill is {small_size}")
+#     elif add_pepperoni == "yes":
+#         medium_size += 3
+#         print(f"your final bill is {medium_size}")
+#     elif add_pepperoni == "yes":
+#         large_size += 3
+#         print(f"your final bill is {large_size}")
+#     else:
+#         print("you are too broke")
+#     extra_cheese = input("Do you want extra cheese? yes or no  ")
+#     if extra_cheese == "yes":
+#         small_size += 1
+#         print(f"your final bill is {small_size}")
+#     elif extra_cheese == "yes":
+#         medium_size += 1
+#         print(f"your final bill is {medium_size}")
+#     elif extra_cheese == "yes":
+#         large_size += 1
+#         print(f"your final bill is {large_size}")
+#     else:
+#         print("you too broke go joor")
+#
+#
+# pizza_deliveries()
