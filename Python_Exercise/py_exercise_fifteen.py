6b775344440ce1180ca9873818fc19a287b7137d
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 18:50:49 +0100

    Add files via upload

--- Python_Exercise/py_exercise_fifteen.py
+++ Python_Exercise/py_exercise_fifteen.py
@@ -2,6 +2,8 @@ def odd_or_even():
     number = int(input("Enter number:  "))
     if number % 2 == 0:
         print(f"{number} is an even number")
+    elif number % 2 != 0:
+        print(f"{number} is an odd number")
 
 
 odd_or_even()
