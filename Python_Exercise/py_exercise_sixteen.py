56194a525a0d88cf85aa97e200cc21f3f56ac388
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 18:54:13 +0100

    Add files via upload

--- Python_Exercise/py_exercise_sixteen.py
+++ Python_Exercise/py_exercise_sixteen.py
@@ -6,4 +6,6 @@ def rollercoaster_two():
         age = int(input("kindly enter your age:  "))
     if age > 18:
         print("your ride would cost you $12")
-   
+    else:
+        print("your ride would cost you $7")
+
