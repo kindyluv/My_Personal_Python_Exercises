d5b32e34ac4d947282c0f667d97700b96655a528
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 19:04:48 +0100

    Add files via upload

--- Python_Exercise/py_exercise_eighteen.py
+++ Python_Exercise/py_exercise_eighteen.py
@@ -5,8 +5,8 @@ def user_bmi():
     bmi = weight/height**2
     if bmi < 18.5:
         print(f"your bmi is {bmi} so you are underweight.")
-#     elif 18.5 < bmi < 25:
-#         print(f"your bmi is {bmi} so you are  normal weight.")
+    elif 18.5 < bmi < 25:
+        print(f"your bmi is {bmi} so you are  normal weight.")
 #     elif 25 < bmi < 30:
 #         print(f"your bmi is {bmi} so you are  overWeight.")
 #     elif 30 < bmi < 35:
