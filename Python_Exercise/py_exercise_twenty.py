f9005b8abf41aa2882eb11ea18409821a03f5fc7
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-11 19:19:51 +0100

    Add files via upload

---
+++ Python_Exercise/py_exercise_twenty.py
@@ -0,0 +1,18 @@
+def leap_year_one():
+    year = int(input("Enter year:  "))
+
+    if year % 4 == 0:
+        print(f"{year} a leap year")
+        if year % 100 == 0:
+            print(f"{year} is not a leap year")
+#         else:
+#             print(f"{year} a leap year")
+#             if year % 400 == 0:
+#                 print(f"{year} a leap year")
+#             else:
+#                 print(f"{year} is not a leap year")
+#     else:
+#         print(f"{year} is not a leap year")
+#
+#
+# leap_year_one()
