1116fe20410b5cffae315798e5d32c1237c5fac3
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-15 23:59:50 +0100

    Add files via upload

--- Python_Exercise/main.py
+++ Python_Exercise/main.py
@@ -52,8 +52,8 @@ elif user_inputs == 12:
     user_inputs = exercise.string_palindrome()
 elif user_inputs == 14:
     user_inputs = exercise.pangram()
-# elif user_inputs == 15:
-#     user_inputs = exercise.hyphen_separated_sequence()
+elif user_inputs == 15:
+    user_inputs = exercise.hyphen_separated_sequence()
 # elif user_inputs == 16:
 #     user_inputs = exercise.value_square_numbers()
 # else:
