2ecce256b8a1bf76f91d23c6e65aa1c45d758cef
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-04 21:20:17 +0100

    Add files via upload

---
+++ Python_Exercise/list/new_employee_dob.py
@@ -0,0 +1,7 @@
+def create_new_employee_dob():
+    employee_dob = []
+
+    emp_dob_input = input('Enter employee date of birth \n')
+    employee_dob.append(emp_dob_input)
+
+    return employee_dob
