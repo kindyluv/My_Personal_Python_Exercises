665d8fcdc33539b8feee7d8219a711c37662e7cc
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-04 21:19:09 +0100

    Add files via upload

---
+++ Python_Exercise/list/list_multiple_assignment.py
@@ -0,0 +1,7 @@
+def list_multiple_Assignment():
+    cat = ['fat', 'black', 'goat', 'crying']
+    size = cat[0]
+    colour = cat[2]
+    disposition = cat[1]
+    name = cat[3]
+    cat.sort(key=str.lower)
