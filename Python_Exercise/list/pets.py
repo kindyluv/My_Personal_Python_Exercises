1eea7659349e6f988da5edb5af73fd71cbe3419d
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-04 21:20:34 +0100

    Add files via upload

---
+++ Python_Exercise/list/pets.py
@@ -0,0 +1,10 @@
+def pets_():
+    my_pets = ['me', 'we', 'them', 'us', 'now', ]
+    print(my_pets)
+    my_pets_two = ['so', 'qw', 'yu', 'gyy']
+    my_pets.sort(reverse=True)
+    print(my_pets)
+    # print(my_pets.index('qw'))
+
+
+pets_()
