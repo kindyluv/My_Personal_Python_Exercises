1b5992a51c6c3ff2b889c61c8944ef9b9f8514ed
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-04 21:18:38 +0100

    Add files via upload

---
+++ Python_Exercise/list/employee_department_list.py
@@ -0,0 +1,9 @@
+def create_new_employee_department():
+    employee_department = []
+    emp = ' '
+    while emp != ' ':
+        emp_department_input = input('Enter employee last name \n')
+
+        employee_department.append(emp_department_input)
+
+    return employee_department
